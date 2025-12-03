0b6094b53c15abd7a3fbe2829a1d9aab2e690531
animesh65432 <kiranduttta234@gmail.com>
2025-12-04 00:34:16 +0530

    fix it

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from data import data
 
 async def main():
     try:
-        # announcements = await scrape_all_states(batch_size=3)
-        # print("scraped announcements from all states",len(announcements))
-        # faiss_service = FaissService(announcements)
-        # unique_announcements = faiss_service.get_unique(threshold=0.90)
-        # print("unique announcements after faiss",len(unique_announcements))
-        # classified_announcements = await classify_announcement_or_news(unique_announcements)
-        # print("classified announcements",classified_announcements)
+        announcements = await scrape_all_states(batch_size=3)
+        print("scraped announcements from all states",len(announcements))
+        faiss_service = FaissService(announcements)
+        unique_announcements = faiss_service.get_unique(threshold=0.90)
+        print("unique announcements after faiss",len(unique_announcements))
+        classified_announcements = await classify_announcement_or_news(unique_announcements)
+        print("classified announcements",classified_announcements)
         announcements_with_pdf_text = await extract_text_from_pdf_bytes(data)
         print("announcements with pdf text extracted",announcements_with_pdf_text)
         print("✅ All tasks completed successfully!",len(announcements_with_pdf_text))
